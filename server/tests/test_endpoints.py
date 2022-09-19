c7a92bf27c9fcf665158ca457537507ad43f46f6
Dayana Alejandro <dayana200120@gmail.com>
2022-09-18 18:38:09 -0700

    updated rootdir

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -5,9 +5,8 @@ import server.endpoints as ep
 
 
 def test_hello():
-    # """
-    # See if Hello works.
-    # """
-    # resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
-    # assert isinstance(resp_json[ep.MESSAGE], str)
-    return True
+    """
+    See if Hello works.
+    """
+    resp_json = TEST_CLIENT.get(ep.HELLO).get_json()
+    assert isinstance(resp_json[ep.MESSAGE], str)
