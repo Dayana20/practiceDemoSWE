a2661386672c667dcf09203b5bfea63e9c910366
Dayana Alejandro <dayana200120@gmail.com>
2022-09-27 09:32:59 -0700

    cleaned up after lint check

--- server/endpoints.py
+++ server/endpoints.py
@@ -84,4 +84,3 @@ class Endpoints(Resource):
         endpoints = ''
         # sorted(rule.rule for rule in api.app.url_map.iter_rules())
         return {"Available endpoints": endpoints}
-        
