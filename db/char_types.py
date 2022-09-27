578c2e10ac7ac0ada711811691c3a56884ab5c09
Dayana Alejandro <dayana200120@gmail.com>
2022-09-27 09:39:43 -0700

    cleaned up after lint check for char_types

--- db/char_types.py
+++ db/char_types.py
@@ -6,7 +6,9 @@ WIZARD = 'Wizard'
 WARRIOR = 'Warrior'
 MAGE = 'Mage'
 
-CHAR_TYPES = {WIZARD: {"health": 7}, WARRIOR: {"health": 7}, MAGE: {"health": 7}}
+CHAR_TYPES = {WIZARD: {"health": 7}, 
+    WARRIOR: {"health": 7}, 
+    MAGE: {"health": 7}}
 
 
 def get_char_types():
