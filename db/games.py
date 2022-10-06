4f2fc1c595f20dc100893d3ee393ac1d8233a596
Dayana Alejandro <dayana200120@gmail.com>
2022-10-06 09:00:59 -0700

    added to db add game + tests

--- db/games.py
+++ db/games.py
@@ -3,11 +3,15 @@ this module encapsulates details about games
 '''
 TEST_GAME_NAME = 'Test game'
 NUM_PLAYERS = 'num_players'
+LEVEL = 'level'
+VIOLENCE = 'violence'
 
+
+REQUIRED_FLDS = [NUM_PLAYERS, LEVEL, VIOLENCE]
 # We expect rhw game database to change frequently
-games = {TEST_GAME_NAME: {NUM_PLAYERS: 7, 'level': 10},
-         'game2': {NUM_PLAYERS: 9, 'level': 9},
-         'game3': {NUM_PLAYERS: 6, 'level': 1}, }
+games = {TEST_GAME_NAME: {NUM_PLAYERS: 7, 'level': 10, VIOLENCE: 2},
+         'game2': {NUM_PLAYERS: 9, 'level': 9, VIOLENCE: 2},
+         'game3': {NUM_PLAYERS: 6, 'level': 1, VIOLENCE: 2}, }
 
 
 def get_games():
@@ -16,6 +20,21 @@ def get_games():
 
 def get_game_details(game):
     return games.get(game, None)
+
+
+def add_game(name, details):
+    if not isinstance(name, str):
+        raise TypeError(f'Wrong type for name: {type(name)=}')
+    if not isinstance(details, dict):
+        raise TypeError(f'Wrong type for details: {type(details)=}')
+    for field in REQUIRED_FLDS:
+        if field not in details:
+            raise ValueError(f'Required {field=}c missing from details')
+    games[name] = details
+
+
+def game_exists(name):
+    return name in games
 
 
 def main():
--- db/tests/test_games.py
+++ db/tests/test_games.py
@@ -1,4 +1,5 @@
 import db.games as gm
+import pytest
 
 def test_get_char_types():
     gms = gm.get_games()
@@ -8,3 +9,22 @@ def test_get_char_types():
 def test_get_char_type_details():
     gm_dets = gm.get_game_details(gm.TEST_GAME_NAME)
     assert isinstance(gm_dets, dict)
+
+def test_add_wrong_name_type():
+    with pytest.raises(TypeError):
+        gm.add_game('a new game',{})
+
+def test_add_wrong_details_type():
+    with pytest.raises(TypeError):
+        gm.add_game('a new game',[])
+
+def test_add_missing_field():
+    with pytest.raises(ValueError):
+        gm.add_game('a new game',{'foo':'bar'})
+
+def test_add_game():
+    details = {}
+    for field in gm.REQUIRED_FLDS:
+        details[field] = 2
+    gm.add_game(gm.TEST_GAME_NAME,details)
+    assert gm.game_exists(gm.TEST_GAME_NAME)
